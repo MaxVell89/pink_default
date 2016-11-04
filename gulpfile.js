1c12b76804920f8e131ae16d36e311792250600f
MaxVell89 <max_vell_89@mail.ru>
2016-11-04 22:42:17 +0300

    Добавил синхронизацию при изменении файлов в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    browserSync = require('browser-sync');
 
 gulp.task('less', function () {
   return gulp.src('less/style.less')
     .pipe(less())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: ''
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'less'], function() {
+  gulp.watch('less/style.less', ['less']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/*js', browserSync.reload);
 });
